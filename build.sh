46b9d77d5c68e1a0a4b8014bc380d619b8283016
Kenshin2438 <2953957899@qq.com>
2022-10-06 01:58:47 +0800

    fix format

--- build.sh
+++ build.sh
@@ -12,8 +12,7 @@ cd public
 # install ripgrep
 yum install wget
 mkdir ripgrep
-wget -qO- https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz | \
-   tar xvzf - --strip-components 1 -C ./ripgrep
+wget -qO- https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz | tar xvzf - --strip-components 1 -C ./ripgrep
 export PATH="./ripgrep/:$PATH"
 
 pyftsubset "font/LXGWWenKaiLite-Regular.ttf" --text=$(rg -e '[\w\d]' -oN --no-filename|sort|uniq|tr -d '\n') --no-hinting
