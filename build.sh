11908d47155dcf7ae9ab8d7f694b532606642479
Kenshin2438 <2953957899@qq.com>
2022-10-05 17:42:44 +0800

    update font

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# pip install -r ./requirements.txt
-hugo -D
-# python3 ./hugo-encryptor.py
+# Build the project.
+hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+origin='LXGWWenKaiLite-Regular.ttf' # 原始字体名称
+optimized='LXGW.woff2' # 压缩后的字体名称，注意需要和 font-face 中定义的字体名一致
+
+pyftsubset "font/$origin" --text=$(rg -e '[\w\d]' -oN --no-filename|sort|uniq|tr -d '\n') --no-hinting
+fonttools ttLib.woff2 compress -o "font/$optimized" "font/${origin/\./\.subset\.}"
+rm "font/${origin/\./\.subset\.}"
