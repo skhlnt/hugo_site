fb43e558d83a38046b4d292ae9f7178cd3d56d5f
Kenshin2438 <2953957899@qq.com>
2022-10-06 02:10:14 +0800

    using yum install ripgrep

--- build.sh
+++ build.sh
@@ -10,13 +10,11 @@ hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 cd public
 
 # install ripgrep
-yum install wget
-mkdir ripgrep
-wget -qO- https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz | tar xvzf - --strip-components 1 -C ./ripgrep
-export PATH="./ripgrep/:$PATH"
+yum install yum-utils
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+yum install ripgrep
 
 pyftsubset "font/LXGWWenKaiLite-Regular.ttf" --text=$(rg -e '[\w\d]' -oN --no-filename|sort|uniq|tr -d '\n') --no-hinting
 fonttools ttLib.woff2 compress -o "font/LXGW.woff2" "font/LXGWWenKaiLite-Regular.subset.ttf"
 
 rm "font/LXGWWenKaiLite-Regular.subset.ttf"
-rm -rf ripgrep
