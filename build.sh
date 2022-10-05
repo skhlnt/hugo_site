6e2144b02c392c056335fa3737361238831cec4d
Kenshin2438 <2953957899@qq.com>
2022-10-05 22:42:40 +0800

    renew command

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 # Build the project.
 hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# 中文字体压缩
+# install fonttools, brotli
 pip3 install fonttools brotli
-sudo apt install ripgrep
+
+# install ripgrep
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+yum install ripgrep
 
 # Go To Public folder
 cd public
