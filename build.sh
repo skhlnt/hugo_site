f7c47660aef7db64baf3e51dbc6b236db251978a
Kenshin2438 <2953957899@qq.com>
2022-10-06 00:57:55 +0800

    add ripgrep

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Build the project.
-hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
-cd public
-
 # install fonttools, brotli
 pip3 install fonttools brotli --user
 export PATH="/vercel/.local/bin/:$PATH"
 
+# Build the project.
+hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+cd public
+
 # install ripgrep
-yum install wget
-mkdir ripgrep
-wget -qO- https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz | \
-   tar xvzf - --strip-components 1 -C ./ripgrep
-export PATH="./ripgrep/:$PATH"
+# yum install wget
+# mkdir ripgrep
+# wget -qO- https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz | \
+#    tar xvzf - --strip-components 1 -C ./ripgrep
+export PATH="../ripgrep/:$PATH"
 
 origin='LXGWWenKaiLite-Regular.ttf' # 原始字体名称
 optimized='LXGW.woff2' # 压缩后的字体名称，注意需要和 font-face 中定义的字体名一致
@@ -21,4 +22,3 @@ pyftsubset "font/$origin" --text=$(rg -e '[\w\d]' -oN --no-filename|sort|uniq|tr
 fonttools ttLib.woff2 compress -o "font/$optimized" "font/${origin/\./\.subset\.}"
 
 rm "font/${origin/\./\.subset\.}"
-rm -rf ripgrep
