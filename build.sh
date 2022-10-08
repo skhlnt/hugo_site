df867b76ebd9102c81aff14bab1a0cd25d5c5457
Kenshin2438 <2953957899@qq.com>
2022-10-08 20:03:31 +0800

    fixed font

--- build.sh
+++ build.sh
@@ -15,4 +15,8 @@ hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 pyftsubset "public/font/LXGWWenKaiLite-Regular.ttf" --text=$(rg -e '[\w\d]' "public/" -oN --no-filename | sort | uniq | tr -d '\n') --no-hinting --timing
 fonttools ttLib.woff2 compress -o "public/font/LXGW.woff2" "public/font/LXGWWenKaiLite-Regular.subset.ttf"
 
-rm "public/font/LXGWWenKaiLite-Regular.subset.ttf"
+cp "public/font/LXGW.woff2" "static/font/LXGW.woff2"
+rm -rf public
+
+# rebuild the project.
+hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
